c54b1900280601fef1684e7fe4c3bdf34990622b
imdadur13 <dir131203@gmail.com>
2026-04-24 16:39:12 +0700

    first commit

---
+++ src/test/java/com/gentara/payment/SpringMicroServicePaymentExerciseAppTests.java
@@ -0,0 +1,13 @@
+package com.gentara.payment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMicroServicePaymentExerciseAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
